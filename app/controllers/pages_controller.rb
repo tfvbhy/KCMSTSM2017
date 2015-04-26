80fe4ab7e2a21ac0ad4968f3108bff78456cdbf2
Michael Ishimoto <mishimot@uci.edu>
2015-04-26 08:45:15 -0700

    Team Leader's View

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,29 +11,26 @@ class PagesController < ApplicationController
         @total_check = Finance.sum("check_amount")
         @total_amount = @total_cash + @total_check
 
-		@team_total_cash = Array.new(Team.all.size, 0);
-		@team_total_check = Array.new(Team.all.size, 0);
-		@team_total_amount = Array.new(Team.all.size, 0);
+		@team_total_cash = Array.new(Team.all.size + 1, 0);
+		@team_total_check = Array.new(Team.all.size + 1, 0);
+		@team_total_amount = Array.new(Team.all.size + 1, 0);
 
-        @leader_total_cash = 0
-        @leader_total_check = 0
-
-        @all_leader_indexes = []
 
         User.all.each do |user|
-          #puts in all leaders index
-		  @team_total_cash[user.team.to_i - 1] += user.finances.sum("cash_amount")
-		  @team_total_check[user.team.to_i - 1] += user.finances.sum("check_amount")
+		  #add all the funds of teams who aren't admins and aren't leaders
+          if !user.leader
+			@team_total_cash[user.team.to_i - 1] += user.finances.sum("cash_amount")
+			@team_total_check[user.team.to_i - 1] += user.finances.sum("check_amount")
+		  #the last "team" is the leaders' funds
+		  else
+			@team_total_cash[Team.all.size] += user.finances.sum("cash_amount")
+			@team_total_check[Team.all.size] += user.finances.sum("check_amount")
+		  end
 		end
-		Team.all.each do |team|
-			@team_total_amount[team.id - 1] = @team_total_cash[team.id - 1] + @team_total_check[team.id - 1]
+		#very...scrappy way of adding the totals
+		for i in 0..Team.all.size
+			@team_total_amount[i] = @team_total_cash[i] + @team_total_check[i]
 		end
-        #works with leaders being "Admins"
-        for i in @all_leader_indexes
-          @leader_total_cash += User.find(i).finances.sum("cash_amount")
-          @leader_total_check += User.find(i).finances.sum("check_amount")
-        end
-        @leader_total_amount = @leader_total_cash + @leader_total_check
       end
     end
   end
@@ -41,22 +38,23 @@ class PagesController < ApplicationController
   def teammanagement
 	if user_signed_in?
       if current_user.leader
-		@member_total_cash = Array.new(current_user.team.size, 0);
-		@member_total_check = Array.new(current_user.team.size, 0);
-		@member_total_amount = Array.new(current_user.team.size, 0);
+	    @users = User.where(:team => Team.find(current_user.team))
+		@total_amount = 0
+		@member_total_amount = Array.new(@users.size, 0)
+	
+		@users.each do |u| 
+		  @member_total_amount[u.id - 1] = u.finances.sum("cash_amount") + u.finances.sum("check_amount")
+		  if u != current_user
+		    @total_amount += @member_total_amount[u.id - 1]
+		  end
+		end
 		
-#		User.find_by_team(current_user.team) do |u|
-#			@member_total_cash[0] = u.finances.sum("cash_amount")
-#			@member_total_check[0] = u.finances.sum("check_amount")
-#			@member_total_amount[0] = @member_total_cash + @member_total_check
-#		end
-		
-		@teammanagement_grid = initialize_grid(User,
-          :name => 'g3',
-          :order => 'users.id',
-          :order_direction => 'desc',
-          :enable_export_to_csv => true,
-          :csv_file_name => 'KCMSTSMUSERS')
+#		@teammanagement_grid = initialize_grid(User,
+#          :name => 'g3',
+#          :order => 'users.id',
+#          :order_direction => 'desc',
+#          :enable_export_to_csv => true,
+#          :csv_file_name => 'KCMSTSMUSERS')
 	  end
 	end
   end
